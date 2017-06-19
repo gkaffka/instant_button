88c1a477da93b8ccb1f0bb59fa9a3aa73556c414
Gabriel Kaffka <gabriel.carvalho@vivareal.com>
2017-06-19 15:59:38 -0300

    Fixes update path

--- button_listen.py
+++ button_listen.py
@@ -9,7 +9,7 @@ while True:
     if button.is_pressed:
         os.system("mpg123 /home/pi/Downloads/sound.mp3")
         if needUpdate:
-            os.system("curl -L -o /home/pi/Downloads/sound.mp3 https://github.com/gkaffka/instant_button/raw/master/sound.mp3")
+            os.system("curl -L -o /home/pi/instant_button/sound.mp3 https://github.com/gkaffka/instant_button/raw/master/sound.mp3")
             needUpdate = False
 
     if button_.is_pressed:
