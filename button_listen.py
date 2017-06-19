861365c7c0ae872dec1d26113ad0cb6c54de3388
Gabriel Kaffka <kaffka@gmail.com>
2017-06-19 01:11:57 +0000

    Adds a second button to update script and sound

--- button_listen.py
+++ button_listen.py
@@ -2,11 +2,15 @@ from gpiozero import Button
 import os
 
 button = Button(2)
+button_ = Button(3)
 needUpdate = True
 
 while True:
-    button.wait_for_press()
-    os.system("mpg123 /home/pi/Downloads/sound.mp3")
-    if needUpdate:
-        os.system("curl -L -o /home/pi/Downloads/sound.mp3 https://github.com/gkaffka/instant_button/raw/master/sound.mp3")
-        needUpdate = False
+    if button.is_pressed:
+        os.system("mpg123 /home/pi/Downloads/sound.mp3")
+        if needUpdate:
+            os.system("curl -L -o /home/pi/Downloads/sound.mp3 https://github.com/gkaffka/instant_button/raw/master/sound.mp3")
+            needUpdate = False
+
+    if button_.is_pressed:
+        os.system("cd /home/pi/instant_button/ && git pull")
